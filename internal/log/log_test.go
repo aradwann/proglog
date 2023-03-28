5f16094b1c1cddc84b73650e090175dcef306085
Ahmed Radwan <aradwann@proton.me>
2023-03-28 16:51:53 +0200

    test log

---
+++ internal/log/log_test.go
@@ -0,0 +1,124 @@
+package log
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	api "github.com/aradwann/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(t *testing.T, log *Log){
+		"append and read a record succeeds": testAppendRead,
+		"offset out of range error":         testOutOfRangeErr,
+		"init with exsiting segments":       testInitExisting,
+		"reader":                            testReader,
+		"truncate":                          testTruncate,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", "store_test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
+
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+
+			fn(t, log)
+		})
+	}
+}
+
+func testAppendRead(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
+}
+
+func testOutOfRangeErr(t *testing.T, log *Log) {
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	require.Error(t, err)
+}
+
+// test that log bootstarps itself from the data stored by prior log instances
+func testInitExisting(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(append)
+		require.NoError(t, err)
+	}
+	require.NoError(t, log.Close())
+
+	off, err := log.LowestOffest()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	off, err = log.HighestOffest()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+
+	n, err := NewLog(log.Dir, log.Config)
+	require.NoError(t, err)
+
+	off, err = n.LowestOffest()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = n.HighestOffest()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+
+}
+
+// test that we can read the full raw log as it's stored on disk
+// so we can snapshot and restore the logs in ~finite state machine~
+func testReader(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+	off, err := log.Append(append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	reader := log.Reader()
+	b, err := io.ReadAll(reader)
+	require.NoError(t, err)
+
+	read := &api.Record{}
+	err = proto.Unmarshal(b[lenWidth:], read)
+	require.NoError(t, err)
+	require.Equal(t, append.Value, read.Value)
+
+}
+
+func testTruncate(t *testing.T, log *Log) {
+	append := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(append)
+		require.NoError(t, err)
+	}
+
+	err := log.Truncate(1)
+	require.NoError(t, err)
+
+	_, err = log.Read(0)
+	require.Error(t, err)
+
+}
