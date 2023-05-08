6665316450c8ea56290b33ced8390d7709c81d7f
Ahmed Radwan <aradwann@proton.me>
2023-05-08 20:23:19 +0300

    update code

--- internal/server/server.go
+++ internal/server/server.go
@@ -14,43 +14,45 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+// START: config_authorizer
+type Config struct {
+	CommitLog  CommitLog
+	Authorizer Authorizer
+}
+
 const (
 	objectWildcard = "*"
 	produceAction  = "produce"
 	consumeAction  = "consume"
 )
 
-type Config struct {
-	CommitLog  CommitLog
-	Authorizer Authorizer
-}
+// END: config_authorizer
 
 type grpcServer struct {
 	*Config
 }
 
-func newgrpcServer(config *Config) (srv *grpcServer, err error) {
-	srv = &grpcServer{
+func newgrpcServer(config *Config) (*grpcServer, error) {
+	srv := &grpcServer{
 		Config: config,
 	}
 	return srv, nil
 }
 
-// END: types
-
-// START: newapi
-func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (*grpc.Server, error) {
-	opts = append(opts,
-		grpc.StreamInterceptor(
-			grpc_middleware.ChainStreamServer(
-				grpc_auth.StreamServerInterceptor(authenticate),
-			)),
-		grpc.UnaryInterceptor(
-			grpc_middleware.ChainUnaryServer(
-				grpc_auth.UnaryServerInterceptor(authenticate),
-			)),
-	)
-
+// START: newgrpcserver_before_auth
+// START: newgrpcserver
+func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (
+	*grpc.Server,
+	error,
+) {
+	// END: newgrpcserver_before_auth
+	opts = append(opts, grpc.StreamInterceptor(
+		grpc_middleware.ChainStreamServer(
+			grpc_auth.StreamServerInterceptor(authenticate),
+		)), grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+		grpc_auth.UnaryServerInterceptor(authenticate),
+	)))
+	// START: newgrpcserver_before_auth
 	gsrv := grpc.NewServer(opts...)
 	srv, err := newgrpcServer(config)
 	if err != nil {
@@ -60,12 +62,14 @@ func NewGRPCServer(config *Config, opts ...grpc.ServerOption) (*grpc.Server, err
 	return gsrv, nil
 }
 
-// END: newapi
+// END: newgrpcserver
+// END: newgrpcserver_before_auth
 
 // START: request_response
+// START: produce_authorize
 func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
 	*api.ProduceResponse, error) {
-	// check if authorized
+	// START_HIGHLIGHT
 	if err := s.Authorizer.Authorize(
 		subject(ctx),
 		objectWildcard,
@@ -73,7 +77,7 @@ func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
 	); err != nil {
 		return nil, err
 	}
-
+	// END_HIGHLIGHT
 	offset, err := s.CommitLog.Append(req.Record)
 	if err != nil {
 		return nil, err
@@ -81,9 +85,12 @@ func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
 	return &api.ProduceResponse{Offset: offset}, nil
 }
 
+// END: produce_authorize
+
+// START: consume_authorize
 func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
 	*api.ConsumeResponse, error) {
-	// check if authorized
+	// START_HIGHLIGHT
 	if err := s.Authorizer.Authorize(
 		subject(ctx),
 		objectWildcard,
@@ -91,7 +98,7 @@ func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
 	); err != nil {
 		return nil, err
 	}
-
+	// END_HIGHLIGHT
 	record, err := s.CommitLog.Read(req.Offset)
 	if err != nil {
 		return nil, err
@@ -99,12 +106,11 @@ func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
 	return &api.ConsumeResponse{Record: record}, nil
 }
 
+// END: consume_authorize
 // END: request_response
 
 // START: stream
-func (s *grpcServer) ProduceStream(
-	stream api.Log_ProduceStreamServer,
-) error {
+func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
 	for {
 		req, err := stream.Recv()
 		if err != nil {
@@ -119,8 +125,6 @@ func (s *grpcServer) ProduceStream(
 		}
 	}
 }
-
-// START: consume_stream
 
 func (s *grpcServer) ConsumeStream(
 	req *api.ConsumeRequest,
@@ -147,7 +151,6 @@ func (s *grpcServer) ConsumeStream(
 	}
 }
 
-// END: consume_stream
 // END: stream
 
 // START: commitlog
@@ -158,18 +161,28 @@ type CommitLog interface {
 
 // END: commitlog
 
+// START: authorizer
 type Authorizer interface {
 	Authorize(subject, object, action string) error
 }
 
-// interceptor that reads the subjects out if the client's cert and writes it to the RPC's context
+// END: authorizer
+
+// START: authenticate
 func authenticate(ctx context.Context) (context.Context, error) {
 	peer, ok := peer.FromContext(ctx)
 	if !ok {
-		return ctx, status.New(codes.Unknown, "couldn't find peer info").Err()
+		return ctx, status.New(
+			codes.Unknown,
+			"couldn't find peer info",
+		).Err()
 	}
+
 	if peer.AuthInfo == nil {
-		return context.WithValue(ctx, subjectContextKey{}, ""), nil
+		return ctx, status.New(
+			codes.Unauthenticated,
+			"no transport security being used",
+		).Err()
 	}
 
 	tlsInfo := peer.AuthInfo.(credentials.TLSInfo)
@@ -184,3 +197,5 @@ func subject(ctx context.Context) string {
 }
 
 type subjectContextKey struct{}
+
+// END: authenticate
